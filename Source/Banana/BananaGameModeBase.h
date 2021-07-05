917cbd994aeaca8e6ef38a9dd24c3bd73374394a
gsc_prukol <gsc.prukol@gmail.com>
2021-07-05 18:48:28 +0300

    Add banana model

---
+++ Source/Banana/BananaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BananaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BANANA_API ABananaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
